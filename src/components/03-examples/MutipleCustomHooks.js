bc776b6f665866c9315cb8871f6f36ba4d3ccc79
Armando <armando.zapien@netlogistik.com>
2022-04-20 13:59:30 -0500

    Second commit

--- src/components/03-examples/MutipleCustomHooks.js
+++ src/components/03-examples/MutipleCustomHooks.js
@@ -28,10 +28,12 @@ export const MutipleCustomHooks = () => {
                     )
                     :
                     (
-                        <blockquote className="blockquote text-center">
+                        <>
+                        <blockquote className="blockquote text-end">
                             <p className="mb-3"> {quote} </p>
                             <footer className="blockquote-footer"> {author} </footer>
                         </blockquote>
+                        </>
                     )
             }
             <button
